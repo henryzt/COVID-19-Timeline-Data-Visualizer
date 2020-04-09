feb17267a8522d86bb7fcd1caf1d8d33e36e155d
Henry Zhang <henryzhang9802@gmail.com>
2020-04-10 00:06:47 +0100

    Add isjeff api

--- script.js
+++ script.js
@@ -2,5 +2,11 @@ var app = new Vue({
   el: "#app",
   data: {
     message: "Test",
+    dataNow: null,
   },
+});
+
+fetch("https://api.covid19uk.live/").then(async (res) => {
+  let data = await res.json();
+  app.dataNow = data.data;
 });
