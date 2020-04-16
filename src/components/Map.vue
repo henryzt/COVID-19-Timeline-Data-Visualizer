07f9bb779d17220859fda259fbde1399d658632f
Davies Xue <zcabxue@ucl.ac.uk>
2020-04-17 01:18:40 +0800

    Add uk map

---
+++ src/components/Map.vue
@@ -0,0 +1,51 @@
+<template>
+    <highcharts :constructor-type="'mapChart'" :options="mapOptions" class="map"></highcharts>
+</template>
+<script>
+    export default {
+        props: {
+            locationsData: {}
+        },
+        data () {
+            return {
+                mapOptions: {
+                    chart: {
+                        map: 'UKmap'
+                    },
+                    title: {
+                        text: 'UK COVID19 Map'
+                    },
+                    mapNavigation: {
+                        enabled: true,
+                        buttonOptions: {
+                            alignTo: 'spacingBox'
+                        }
+                    },
+                    colorAxis: {
+                        enables: true,
+                        min: 0
+                    },
+                    series: [{
+                        name: '',
+                        states: {
+                            hover: {
+                                color: '#BADA55'
+                            }
+                        },
+                        dataLabels: {
+                            enabled: false,
+                            format: '{point.name}'
+                        },
+                        allAreas: true,
+                        data: this.locationsData
+                    }]
+                }
+            }
+        }
+    }
+</script>
+<style scoped>
+    .map {
+        min-height: 500px;
+    }
+</style>
--- src/main.js
+++ src/main.js
@@ -2,10 +2,17 @@ import Vue from 'vue'
 import App from './App.vue'
 import scrollSpy, {Easing} from 'vue2-scrollspy';
 import {i18n} from './plugins/i18n';
+import Highcharts from 'highcharts';
+import mapInit from 'highcharts/modules/map'
+import HighchartsVue from 'highcharts-vue';
+import addUKMap from "./js/ukmap";
 
 Vue.use(scrollSpy, {
   easing: Easing.Quartic.InOut
 });
+mapInit(Highcharts);
+addUKMap(Highcharts);
+Vue.use(HighchartsVue);
 
 Vue.config.productionTip = false;
 
