288a96243c5b3af2cf4ff8709f269b22fd60a8f6
Henry Zhang <henryzhang9802@gmail.com>
2021-06-05 20:30:07 +0800

    Update i18n config

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,29 @@
 <template>
+  <div class="covid-header">
+    <div class="header-title">
+      <h2>COVID-19</h2>
+      <h3>{{$t('title')}}</h3>
+    </div>
+  </div>
   <MainNumbers />
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
-import MainNumbers from './components/MainNumbers.vue'
-import {
-  getAllCountryData,
-  getTimeSeries
-} from './utils/api'
+import { defineComponent } from "vue";
+import MainNumbers from "./components/MainNumbers.vue";
+import { getAllCountryData, getTimeSeries } from "./utils/api";
 
 export default defineComponent({
-  name: 'App',
+  name: "App",
   components: {
-    MainNumbers
+    MainNumbers,
   },
-  async mounted(){
-    const counrtyData = await getAllCountryCurrentData();
-    console.log(counrtyData)
-  }
-})
+  async mounted() {
+    const countryData = await getAllCountryData();
+    console.log(countryData);
+  },
+});
 </script>
 
 <style>
-
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,34 @@
 import { createApp } from 'vue'
+import { ElButton, ElSelect } from 'element-plus';
 import App from './App.vue'
+import { createI18n } from 'vue-i18n'
 
-createApp(App).mount('#app')
+import 'element-plus/packages/theme-chalk/src/base.scss'
+
+import zh from "./locales/zh.json";
+import en from "./locales/en.json";
+
+
+const app = createApp(App)
+
+app.component(ElButton.name, ElButton);
+app.component(ElSelect.name, ElSelect);
+
+
+const messages = {
+  "zh": zh,
+  "en": en,
+};
+
+const i18n = createI18n({
+  legacy: false,
+  locale: 'en',
+  fallbackLocale: 'zh',
+  globalInjection: true,
+  messages,
+})
+
+app.use(i18n)
+
+
+app.mount('#app')
