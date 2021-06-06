8a4ec25040674644aee7382b70eef0a6788f7347
Henry Zhang <henryzhang9802@gmail.com>
2021-06-06 14:40:47 +0800

    Add overview display

--- src/components/MainNumbers.vue
+++ src/components/MainNumbers.vue
@@ -1,26 +1,54 @@
 <template>
   <div class="block">
-
+    <div v-for="(item, idx) of display" :key="idx">
+      <div v-if="item">{{ item.title }}|{{ item.today }}</div>
+    </div>
   </div>
-
 </template>
 
 <script lang="ts">
-import { ref, defineComponent } from 'vue'
+import { ref, defineComponent } from "vue";
 export default defineComponent({
-  name: 'MainNumbers',
+  name: "MainNumbers",
   props: {
     overviewData: {
       type: Object,
-      required: true
+      required: true,
+    },
+  },
+  data() {
+    return {
+      display: [null, null, null, null]
     }
   },
-  setup(props) {
-    console.log(props.overviewData)
-  }
-})
+  watch: {
+    overviewData(data) {
+      console.log(data);
+      const cases = {
+        title: "Cases",
+        today: data.todayCases,
+        total: data.cases,
+      };
+      const deaths = {
+        title: "Deaths",
+        today: data.todayDeaths,
+        total: data.deaths,
+      };
+      const active = {
+        title: "Active",
+        critical: data.critical,
+        total: data.active,
+      };
+      const recovered = {
+        title: "Recovered",
+        today: data.todayRecovered,
+        total: data.recovered,
+      };
+      this.display = [cases, deaths, active, recovered];
+    }
+  },
+});
 </script>
 
 <style scoped>
-
 </style>
