0170fb5e4cae6450db48a575efa99506e96903ea
Henry Zhang <henryzhang9802@gmail.com>
2021-06-06 15:48:17 +0800

    Style overview

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div class="covid-header">
+    <div class="header">
       <div class="header-title">
         <h2>COVID-19</h2>
         <h3>{{ $t("title") }}</h3>
@@ -16,4 +16,20 @@ export default {
 </script>
 
 <style scoped>
+h6 {
+  padding-left: 20px;
+}
+
+.header {
+  padding: 20px 10px;
+  text-align: left;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.header-title {
+  opacity: 0.5;
+}
+
 </style>
--- src/components/MainNumbers.vue
+++ src/components/MainNumbers.vue
@@ -3,12 +3,12 @@
     <div class="block overview" v-for="(item, idx) of display" :key="idx">
       <div v-if="item">
         <div :class="{ [item.color]: true }">
-          <div class="number-title">Today</div>
+          <div class="overview-title">{{item.title}}</div>
           <div class="number" ref="num">
             {{ $padNum(item.today ?? item.critical) }}
           </div>
           <div class="number-title">Total</div>
-          <div class="number">
+          <div class="number total">
             {{ $padNum(item.total) }}
           </div>
         </div>
@@ -37,25 +37,25 @@ export default defineComponent({
       console.log(data);
       const cases = {
         color: "red",
-        title: "Cases",
+        title: "Cases Today",
         today: data.todayCases,
         total: data.cases,
       };
       const deaths = {
         color: "black",
-        title: "Deaths",
+        title: "Deaths Today",
         today: data.todayDeaths,
         total: data.deaths,
       };
       const active = {
         color: "orange",
-        title: "Active",
+        title: "Active Today",
         critical: data.critical,
         total: data.active,
       };
       const recovered = {
         color: "blue",
-        title: "Recovered",
+        title: "Recovered Today",
         today: data.todayRecovered,
         total: data.recovered,
       };
@@ -78,12 +78,27 @@ export default defineComponent({
   justify-content: space-between;
   width: 48%;
   margin-bottom: 30px;
+  font-weight: bold;
+}
+
+.overview-title{
+  font-size:1em;
+  opacity:0.5;
+}
+
+.number-title {
+  opacity:0.5;
 }
 
 .number {
   font-size: 1.7em;
   padding-bottom: 10px;
 }
+
+.total {
+  font-size: 1.2em;
+}
+
 .red .number {
   color: var(--red);
 }
