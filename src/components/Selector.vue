038687512208699761e90c6d6c257bd0bac7f360
Henry Zhang <henryzhang9802@gmail.com>
2021-06-13 14:36:06 +0800

    Add overflow selector

--- src/components/Selector.vue
+++ src/components/Selector.vue
@@ -2,43 +2,86 @@
   <div>
     <n-button-group>
       <n-button
-        v-for="type in types"
+        v-for="type in types.slice(0, limit)"
         :key="type"
         round
         ghost
-        :type="modelValue == type ? 'primary':'default'"
+        :type="modelValue == type ? 'primary' : 'default'"
         size="small"
         @click="changeType(type)"
       >
         {{ type }}
       </n-button>
+      <!-- overflow -->
+
+      <n-dropdown
+        trigger="hover"
+        @select="changeType"
+        placement="bottom-start"
+        :options="dropdownOptions"
+      >
+        <n-button
+          round
+          ghost
+          size="small"
+          :type="isOverflowActive ? 'primary' : 'default'"
+        >
+          <n-icon size="15">
+            <ChevronDown />
+          </n-icon>
+          <span v-if="isOverflowActive">{{modelValue}}</span>
+        </n-button>
+      </n-dropdown>
     </n-button-group>
   </div>
 </template>
 
 <script>
-import { NButton, NButtonGroup } from "naive-ui";
+import { ChevronDown } from "@vicons/ionicons5";
+import { NButton, NButtonGroup, NIcon, NDropdown } from "naive-ui";
 
 export default {
   name: "Selector",
   props: {
+    limit: {
+      type: Number,
+      default: 3,
+    },
     types: {
       type: Array,
       default: [],
     },
     modelValue: {
       type: String,
-      default: ""
-    }
+      default: "",
+    },
   },
   methods: {
     changeType(e) {
-      this.$emit('update:modelValue', e);
-    }
+      this.$emit("update:modelValue", e);
+    },
   },
   components: {
     NButton,
     NButtonGroup,
+    NIcon,
+    ChevronDown,
+    NDropdown,
+  },
+  computed: {
+    dropdownOptions() {
+      const overflowData = this.types.slice(this.limit);
+      const options = overflowData.map((i) => {
+        return {
+          key: i,
+          label: i,
+        };
+      });
+      return options;
+    },
+    isOverflowActive() {
+      return this.dropdownOptions.map((i) => i.key).includes(this.modelValue);
+    },
   },
 };
 </script>
