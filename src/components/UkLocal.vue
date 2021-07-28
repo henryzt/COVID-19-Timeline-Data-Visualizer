64cfa537d47bd6dd37a7535cbdab0fdab6a565cd
Henry Zhang <henryzhang9802@gmail.com>
2021-07-29 01:21:23 +0800

    Add local uk api

--- src/components/UkLocal.vue
+++ src/components/UkLocal.vue
@@ -11,25 +11,55 @@ export default {
   name: "UkLocal",
   data() {
     return {
-      dataUk: null,
       nation: null,
-      ulta: null,
+      utla: null,
       display: null,
-      chartData: null
+      chartData: null,
+      overview: null
     };
   },
-  mounted() {
-    fetch("https://uk.henryzt.com/covid/uk.php").then(async (res) => {
-      let data = await res.json();
-      this.dataUk = data;
-      this.nation = data.nation.data;
-      this.utla = data.utla.data;
-      this.loadUkRealtimeDisplay()
-    });
+  async mounted() {
+    const queries = `{
+        "date":"date",
+        "areaName":"areaName",
+        "areaCode":"areaCode",
+        "admissions":"cumAdmissions",
+        "admissionsNew":"newAdmissions",
+        "hospitalCases":"hospitalCases",
+        "covidOccupiedMVBeds":"covidOccupiedMVBeds",
+        "confirmedRate":"cumCasesByPublishDateRate",
+        "confirmedNew":"newCasesByPublishDate",
+        "confirmedNewBySpecimen":"newCasesBySpecimenDate",
+        "confirmed":"cumCasesByPublishDate",
+        "confirmedBySpecimen":"cumCasesBySpecimenDate",
+        "deathNewBySpecimen":"newDeaths28DaysByDeathDate",
+        "deathNew":"newDeaths28DaysByPublishDate",
+        "death":"cumDeaths28DaysByPublishDate",
+        "deathRate":"cumDeaths28DaysByPublishDateRate",
+        "testedNew":"newTestsByPublishDate",
+        "tested":"cumTestsByPublishDate"
+    }`.replaceAll(" ", "").replaceAll("\n", "");
+
+    const structure = "&structure=" + encodeURIComponent(queries);
+    const baseUrl = "https://api.coronavirus.data.gov.uk/v1/data?filters=areaType=";
+
+    const nationRes = await fetch(`${baseUrl}nation${structure}`);
+    const nation = await nationRes.json();
+    this.nation = nation.data;
+
+    const overviewRes = await fetch(`${baseUrl}overview${structure}`);
+    const overview = await overviewRes.json();
+    this.overview = overview.data;
+
+    this.loadUkRealtimeDisplay();
+
+    const utlaRes = await fetch(`${baseUrl}utla&latestBy=newCasesByPublishDate${structure}`);
+    const utla = await utlaRes.json();
+    this.utla = utla.data;
   },
   methods: {
     loadUkRealtimeDisplay() {
-      let latest = this.dataUk.latest;
+      let latest = this.overview[0];
       this.display = {
         confirmed: latest.confirmed,
         confirmedChange: latest.confirmedNew,
@@ -38,7 +68,7 @@ export default {
         admission: latest.hospitalCases,
         admissionChange: latest.admissionsNew,
       };
-      this.chartData = this.dataUk.overview.data.reverse();
+      this.chartData = this.overview.reverse();
     },
   },
   components: {
