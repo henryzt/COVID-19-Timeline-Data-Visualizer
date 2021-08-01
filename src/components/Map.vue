715dc63f8c25b3a827abf8a041d1ee14907c21d1
Henry Zhang <henryzhang9802@gmail.com>
2021-08-01 12:37:50 +0800

    Add global and uk maps

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -41,6 +41,10 @@ export default {
       type: String,
       default: "total",
     },
+    country: {
+      type: String,
+      default: "all",
+    },
   },
   data() {
     return {
@@ -50,34 +54,46 @@ export default {
   mounted() {
     this.register();
   },
+  computed: {
+    name() {
+      return this.data;
+    },
+  },
   methods: {
     register() {
-      fetch("/maps/usa.json")
+      fetch("/maps/global.json")
         .then((response) => response.json())
         .then((mapJson) => {
-          registerMap("USA", mapJson, {
-            Alaska: {
-              left: -131,
-              top: 25,
-              width: 15,
-            },
-            Hawaii: {
-              left: -110, // 夏威夷
-              top: 28,
-              width: 5,
-            },
-            "Puerto Rico": {
-              // 波多黎各
-              left: -76,
-              top: 26,
-              width: 2,
-            },
-          });
+          let offset = null;
+          if (this.country === "usa") {
+            offset = {
+              Alaska: {
+                left: -131,
+                top: 25,
+                width: 15,
+              },
+              Hawaii: {
+                left: -110,
+                top: 28,
+                width: 5,
+              },
+              "Puerto Rico": {
+                left: -76,
+                top: 26,
+                width: 2,
+              },
+            };
+          }
+          registerMap(this.country, mapJson, offset);
           this.updateChart();
         });
     },
     updateChart() {
       if (!this.tableData) return;
+      const mapData = this.tableData.map((e)=>({
+        name: e.locationName,
+        value: e[this.dataType]
+      }))
       this.option = {
         title: {
           text: "USA Population Estimates (2012)",
@@ -133,70 +149,13 @@ export default {
             name: "USA PopEstimates",
             type: "map",
             roam: true,
-            map: "USA",
+            map: this.country,
             emphasis: {
               label: {
                 show: true,
               },
             },
-            // 文本位置修正
-            textFixed: {
-              Alaska: [20, -20],
-            },
-            data: [
-              { name: "Alabama", value: 4822023 },
-              { name: "Alaska", value: 731449 },
-              { name: "Arizona", value: 6553255 },
-              { name: "Arkansas", value: 2949131 },
-              { name: "California", value: 38041430 },
-              { name: "Colorado", value: 5187582 },
-              { name: "Connecticut", value: 3590347 },
-              { name: "Delaware", value: 917092 },
-              { name: "District of Columbia", value: 632323 },
-              { name: "Florida", value: 19317568 },
-              { name: "Georgia", value: 9919945 },
-              { name: "Hawaii", value: 1392313 },
-              { name: "Idaho", value: 1595728 },
-              { name: "Illinois", value: 12875255 },
-              { name: "Indiana", value: 6537334 },
-              { name: "Iowa", value: 3074186 },
-              { name: "Kansas", value: 2885905 },
-              { name: "Kentucky", value: 4380415 },
-              { name: "Louisiana", value: 4601893 },
-              { name: "Maine", value: 1329192 },
-              { name: "Maryland", value: 5884563 },
-              { name: "Massachusetts", value: 6646144 },
-              { name: "Michigan", value: 9883360 },
-              { name: "Minnesota", value: 5379139 },
-              { name: "Mississippi", value: 2984926 },
-              { name: "Missouri", value: 6021988 },
-              { name: "Montana", value: 1005141 },
-              { name: "Nebraska", value: 1855525 },
-              { name: "Nevada", value: 2758931 },
-              { name: "New Hampshire", value: 1320718 },
-              { name: "New Jersey", value: 8864590 },
-              { name: "New Mexico", value: 2085538 },
-              { name: "New York", value: 19570261 },
-              { name: "North Carolina", value: 9752073 },
-              { name: "North Dakota", value: 699628 },
-              { name: "Ohio", value: 11544225 },
-              { name: "Oklahoma", value: 3814820 },
-              { name: "Oregon", value: 3899353 },
-              { name: "Pennsylvania", value: 12763536 },
-              { name: "Rhode Island", value: 1050292 },
-              { name: "South Carolina", value: 4723723 },
-              { name: "South Dakota", value: 833354 },
-              { name: "Tennessee", value: 6456243 },
-              { name: "Texas", value: 26059203 },
-              { name: "Utah", value: 2855287 },
-              { name: "Vermont", value: 626011 },
-              { name: "Virginia", value: 8185867 },
-              { name: "Washington", value: 6897012 },
-              { name: "West Virginia", value: 1855413 },
-              { name: "Wisconsin", value: 5726398 },
-              { name: "Wyoming", value: 576412 },
-              { name: "Puerto Rico", value: 3667084 },
-            ],
+            data: mapData,
           },
         ],
       };
